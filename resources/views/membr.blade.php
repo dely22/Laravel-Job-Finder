9dd77ee1fdcf64bd90b813a1d199f3ea7958c07f
dely22 <almktaryh@gmail.com>
2022-04-04 02:37:55 -0800

    add views to the proj

---
+++ resources/views/membr.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Members</title>
+
+    
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/boxicons.min.css" rel="stylesheet">
+    <link href="../css/custom.css" rel="stylesheet">
+    <link href="../css/footer.css" rel="stylesheet">
+</head>
+<body>
+
+      <!-- top nav start -->
+        <div class="row mx-1 bg-white my-3 col-12 d-flex justify-content-lg-between shadow">
+            <nav aria-label="breadcrumb" class="main-breadcrumb col-6 p-3">
+              <ol class="breadcrumb ms-3" >
+                <li class="breadcrumb-item fs-6 fw-bold"><a href="../index.html">Home</a></li>
+                <li class="breadcrumb-item active fs-6 fw-bold" aria-current="page"> Job Finder Members</li>
+              </ol>
+              
+            </nav>
+         
+           
+        </div>
+         <!-- top nav end -->
+
+        <!-- MeMbers -->
+   <section class="container mb-2">
+     <div class="text-center mb-5">
+        <h2 class="section-heading justify-content-center text-uppercase text-center text-danger">Job Finder Members</h2>
+      </div>
+      <div class="row">
+        <!-- card -->
+        <div class="col-lg-4 col-md-10 mb-2 shadow">
+            <div class="card p-3 mb-2">
+                <div class="text-center mt-2 p-3"> 
+                    <img src="../img/nizoch.jpg" width="100" /> 
+                    <div class="text-center text-danger"> Hadeel Jameel </div>
+                    <span class="d-block font-weight-bold ">Programmer</span>
+                    <hr class="text-danger"> 
+                    <span>IBM Inc</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                        <small class="ml-1 text-break"> Taiz</small> 
+                    </div>
+                    <div class="d-flex flex-column align-content-center mt-2 shadow">
+                        <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+          <!-- card -->
+        <div class="col-lg-4 col-md-10 mb-2 shadow">
+            <div class="card p-3 mb-2">
+                <div class="text-center mt-2 p-3"> 
+                    <img src="../img/nizoch.jpg" width="100" /> 
+                    <div class="text-center text-danger"> Hadeel Jameel </div>
+                    <span class="d-block font-weight-bold ">Programmer</span>
+                    <hr class="text-danger"> 
+                    <span>IBM Inc</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                        <small class="ml-1 text-break"> Taiz</small> 
+                    </div>
+                    <div class="d-flex flex-column align-content-center mt-2 shadow">
+                        <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+          <!-- card -->
+        <div class="col-lg-4 col-md-10 mb-2 shadow">
+            <div class="card p-3 mb-2">
+                <div class="text-center mt-2 p-3"> 
+                    <img src="../img/nizoch.jpg" width="100" /> 
+                    <div class="text-center text-danger"> Hadeel Jameel </div>
+                    <span class="d-block font-weight-bold ">Programmer</span>
+                    <hr class="text-danger"> 
+                    <span>IBM Inc</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                        <small class="ml-1 text-break"> Taiz</small> 
+                    </div>
+                    <div class="d-flex flex-column align-content-center mt-2 shadow">
+                        <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+          <!-- card -->
+        <div class="col-lg-4 col-md-10 mb-2 shadow">
+            <div class="card p-3 mb-2">
+                <div class="text-center mt-2 p-3"> 
+                    <img src="../img/nizoch.jpg" width="100" /> 
+                    <div class="text-center text-danger"> Hadeel Jameel </div>
+                    <span class="d-block font-weight-bold ">Programmer</span>
+                    <hr class="text-danger"> 
+                    <span>IBM Inc</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                        <small class="ml-1 text-break"> Taiz</small> 
+                    </div>
+                    <div class="d-flex flex-column align-content-center mt-2 shadow">
+                        <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+          <!-- card -->
+        <div class="col-lg-4 col-md-10 mb-2 shadow">
+            <div class="card p-3 mb-2">
+                <div class="text-center mt-2 p-3"> 
+                    <img src="../img/nizoch.jpg" width="100" /> 
+                    <div class="text-center text-danger"> Hadeel Jameel </div>
+                    <span class="d-block font-weight-bold ">Programmer</span>
+                    <hr class="text-danger"> 
+                    <span>IBM Inc</span>
+                    <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                        <small class="ml-1 text-break"> Taiz</small> 
+                    </div>
+                    <div class="d-flex flex-column align-content-center mt-2 shadow">
+                        <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!-- card -->
+          <div class="col-lg-4 col-md-10 mb-2 shadow">
+              <div class="card p-3 mb-2">
+                  <div class="text-center mt-2 p-3"> 
+                      <img src="../img/nizoch.jpg" width="100" /> 
+                      <div class="text-center text-danger"> Hadeel Jameel </div>
+                      <span class="d-block font-weight-bold ">Programmer</span>
+                      <hr class="text-danger"> 
+                      <span>IBM Inc</span>
+                      <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> 
+                          <small class="ml-1 text-break"> Taiz</small> 
+                      </div>
+                      <div class="d-flex flex-column align-content-center mt-2 shadow">
+                            <a href="#" class="btn btn-sm text-danger btn-outline-dark mt-3">Download CV</a> 
+                      </div>
+                  </div>
+              </div>
+          </div>
+        
+
+        </div>
+      
+    </section>
+    
+</body>
+</html>
